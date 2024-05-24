0e10fb80f5b1a70750ef16ce7e14db8196a1ee0f
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-05-24 20:31:04 +0300

    Install the Cviebrock\Sluggable Package to the project

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Travel extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable; // The Cviebrock\Sluggable Package is for making the 'slug' field a unique field in the table. 
 
     protected $table = "travels"; // because the travel model name is irregular word and doesn't accept the plural form, so we manually add the 's' in the table name.
 
@@ -18,4 +19,13 @@ class Travel extends Model
         'description',
         'number_of_days',
     ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name', // specifying the source of the slug, to get the 'slug' from the 'name' field and it will be a unique slug.
+            ]
+        ];
+    }
 }
