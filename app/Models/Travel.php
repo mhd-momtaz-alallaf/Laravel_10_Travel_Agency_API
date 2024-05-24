3b45cc6c20296427fd77bac4a83143b3921958db
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-05-24 20:48:42 +0300

    Create numperOfNights Accessor in the Travel model

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -27,5 +28,12 @@ class Travel extends Model
                 'source' => 'name', // specifying the source of the slug, to get the 'slug' from the 'name' field and it will be a unique slug.
             ]
         ];
+    }
+
+    public function numperOfNights(): Attribute // this is the new laravel 10 syntax of the Accessor, and this accessor is to auto compute the number of nights from 'number_of_days' field.
+    {
+        return Attribute::make(
+            get: fn ($value, $attributes) => $attributes['number_of_days'] - 1
+        );
     }
 }
