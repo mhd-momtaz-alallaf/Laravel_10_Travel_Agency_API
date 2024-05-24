e731bb04eb54491c07914875905bcce1880f95cb
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-05-24 19:41:33 +0300

    Create role_user pivot table

---
+++ database/migrations/2024_05_24_163334_create_role_user_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('role_user', function (Blueprint $table) { // the migration of the pivot table between roles and users should be written in lowercase alfabet order exactly like this (create_role_user_table).
+            $table->foreignId('role_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('role_user');
+    }
+};
