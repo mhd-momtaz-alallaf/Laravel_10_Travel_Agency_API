308a8cec36b23a1405e52cd1c9aec6c94003fdc7
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-05-25 18:08:30 +0300

    Create the second test function in the ToursListTest file

--- tests/Feature/TourListTest.php
+++ tests/Feature/TourListTest.php
@@ -25,4 +25,21 @@ class TourListTest extends TestCase
         $response->assertJsonCount(1, 'data'); // assert have only one record.
         $response->assertJsonFragment(['id'=> $tour->id]); // to see if the id of that tour is exist.
     }
+
+    public function test_tour_price_is_shown_correctly(): void
+    {
+
+        $travel = Travel::factory()->create(); // create 1 travel.
+
+        $tour = Tour::factory()->create([ // create 1 tour associated with above travel.
+            'travel_id' => $travel->id,
+            'price' => 123.45,
+        ]);
+
+        $response = $this->get('/api/v1/travels/'. $travel->slug .'/tours'); // navigate to the tour route.
+
+        $response->assertStatus(200); // assert getting data from the route (the route is exist and correct).
+        $response->assertJsonCount(1, 'data'); // assert have only one record.
+        $response->assertJsonFragment(['price'=> 123.45]); // to see if the price of that tour is correct.
+    }
 }
