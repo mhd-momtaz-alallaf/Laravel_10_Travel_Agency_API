5b50c2db4eb7691e09f0368e561df9dcfb15e7f7
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-05-25 18:03:27 +0300

    Create the ToursListTest Feature Test with the first test function

---
+++ tests/Feature/TourListTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Tour;
+use App\Models\Travel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TourListTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_tour_list_by_travel_slug_returns_correct_tours(): void
+    {
+
+        $travel = Travel::factory()->create(); // create 1 travel.
+
+        $tour = Tour::factory()->create(['travel_id' => $travel->id]); // create 1 tour associated with above travel.
+
+        $response = $this->get('/api/v1/travels/'. $travel->slug .'/tours'); // navigate to the tour route.
+
+        $response->assertStatus(200); // assert getting data from the route (the route is exist and correct).
+        $response->assertJsonCount(1, 'data'); // assert have only one record.
+        $response->assertJsonFragment(['id'=> $tour->id]); // to see if the id of that tour is exist.
+    }
+}
