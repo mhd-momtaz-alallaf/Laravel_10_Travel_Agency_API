78d38c18c039d8c3b48eef043d9de69bd7e3b05a
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-05-25 20:00:18 +0300

    Adding the ability to filtering the tours by the Date and Price in the TourController

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -9,9 +9,21 @@ use Illuminate\Http\Request;
 
 class TourController extends Controller
 {
-    public function index(Travel $travel) // the route model binding is travel because the tour is scoped by the travel (there is not tour without travel).
+    public function index(Travel $travel, Request $request) // the route model binding is travel because the tour is scoped by the travel (there is not tour without travel).
     {
         $tours = $travel->tours()
+            ->when($request->priceFrom, function ($query) use ($request) { // to filter by tour price, via the priceFrom optional route parameter.
+                $query->where('price', '>=', $request->priceFrom * 100); // priceFrom * 100 to have the right Matching with the database price because the price in the database is * 100.
+            })
+            ->when($request->priceTo, function ($query) use ($request) { // to filter by priceTo optional route parameter.
+                $query->where('price', '<=', $request->priceTo * 100);
+            })
+            ->when($request->dateFrom, function ($query) use ($request) { // to filter by dateFrom optional route parameter.
+                $query->where('starting_date', '>=', $request->dateFrom);
+            })
+            ->when($request->dateTo, function ($query) use ($request) { // to filter by dateTo route optional parameter.
+                $query->where('starting_date', '<=', $request->dateTo);
+            })
             ->orderBy('starting_date')
             ->paginate();
 
