d14ed19f203e614ba775218c61fce67edcbc3a1c
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-05-25 13:09:32 +0300

    Create TravelResource and use it in the TravelController index method

--- app/Http/Controllers/Api/V1/TravelController.php
+++ app/Http/Controllers/Api/V1/TravelController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TravelResource;
 use App\Models\Travel;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,8 @@ class TravelController extends Controller
 {
     public function index()
     {
-        return Travel::where('is_public', true)->get(); // to get the list of only public travels.
+        $travels = Travel::where('is_public', true)->get(); // to get the list of only public travels.
+
+        return TravelResource::collection($travels); 
     }
 }
