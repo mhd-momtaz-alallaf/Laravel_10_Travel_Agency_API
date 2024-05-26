1cc9c83ec8bcabff1a3db50bf53fb984cd685bbc
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-05-26 22:19:48 +0300

    Create the second test function in the LoginTest

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -23,4 +23,16 @@ class LoginTest extends TestCase
         $response->assertStatus(200); // assert that the response is ok.
         $response->assertJsonStructure(['access_token']); // assert the response returns the access_token.
     }
+
+    public function test_login_returns_error_with_invalid_credentials(): void
+    {
+        // we will not create a user this time => invalid credentials.
+
+        $response = $this->postJson('/api/v1/login',[ // navigate to the login route, passing invalid credentials.
+            'email'=> 'invalid@email.com',
+            'password'=> 'password',
+        ]);
+
+        $response->assertStatus(422); // assert that the response returns validation error 422.
+    }
 }
