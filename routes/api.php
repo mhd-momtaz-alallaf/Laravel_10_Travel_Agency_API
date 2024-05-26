c7c85e3ea95f16e3a8e5f500a3ba3a2e2f431a1e
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-05-26 18:23:18 +0300

    Protecting the routes with the auth:sanctum middleware and adding the login route to the api.php file

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\Admin\TravelController as AdminTravelController;
+use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\TourController;
 use App\Http\Controllers\Api\V1\TravelController;
 use Illuminate\Http\Request;
@@ -24,7 +25,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/travels',[TravelController::class, 'index']); // to get the list of public travels.
 Route::get('/travels/{travel:slug}/tours',[TourController::class,'index']); // to get the list of a travel tours, travel:slug means the search id of the travel will be the slug (/travels/first-travel/tours).
 
-Route::prefix('/admin')->group(function () { // after adding this prefix, the routes of this group will be api/v1/admin/...
+Route::prefix('/admin')->middleware('auth:sanctum')->group(function () { // after adding this prefix, the routes of this group will be api/v1/admin/...
     Route::post('/travels', [AdminTravelController::class, 'store']); // to post (create) new travel.
-
 });
+
+Route::post('/login', LoginController::class); // The login route, we don't have to call the method by name because its a __invoke method inside the LoginController.
