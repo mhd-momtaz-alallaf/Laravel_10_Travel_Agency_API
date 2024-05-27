08a34bda6c397b528c08ebc798f909188efe3338
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-05-27 21:43:07 +0300

    Create update method in Admin TravelController with UpdateTravelRequest

--- app/Http/Controllers/Api/V1/Admin/TravelController.php
+++ app/Http/Controllers/Api/V1/Admin/TravelController.php
@@ -4,14 +4,22 @@ namespace App\Http\Controllers\Api\V1\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TravelRequest;
+use App\Http\Requests\UpdateTravelRequest;
 use App\Http\Resources\TravelResource;
 use App\Models\Travel;
 
 class TravelController extends Controller
 {
-    public function store(TravelRequest $request)
+    public function store(TravelRequest $request) // this method is for creating a new travel by the admin.
     {
         $travel = Travel::create($request->validated());
+
+        return new TravelResource($travel);
+    }
+
+    public function update(Travel $travel, UpdateTravelRequest $request) // this method is for Editing an existing travel by the admin or the editor (RoleMiddleware will be applied).
+    {
+        $travel->update($request->validated());
 
         return new TravelResource($travel);
     }
