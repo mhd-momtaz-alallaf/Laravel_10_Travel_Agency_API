ca00cd3e8dcfb1f5c93ebf9146e0d7c3a34efb6f
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-05-27 19:21:52 +0300

    Create the second test function in the AdminTourTest

--- tests/Feature/AdminTourTest.php
+++ tests/Feature/AdminTourTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Models\Role;
 use App\Models\Travel;
+use App\Models\User;
+use Database\Seeders\RoleSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -18,5 +21,20 @@ class AdminTourTest extends TestCase
         $response = $this->postJson('/api/v1/admin/travels/'.$travel->id.'/tours'); // navigate to the create travel tours route without login.
 
         $response->assertStatus(401); // assert getting unauthenticated code.
+    }
+
+    public function test_non_admin_user_cannot_access_adding_tour_route(): void
+    {
+        $this->seed(RoleSeeder::class); // to seed the roles into the testing database.
+
+        $user = User::factory()->create(); // create a new user.
+
+        $user->roles()->attach(Role::where('name' ,'editor')->value('id')); // attach the editor role id to the created user.
+        
+        $travel  = Travel::factory()->create(); // create a travel.
+
+        $response = $this->actingAs($user)->postJson('/api/v1/admin/travels/'.$travel->id.'/tours'); // navigate to the create travel tours route by the user who having the editor role.
+
+        $response->assertStatus(403); // assert getting forbidden (unauthorized) code.
     }
 }
